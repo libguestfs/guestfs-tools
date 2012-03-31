44e04a6fca6af613b7270db99e8b6521cccb944a
Richard W.M. Jones <rjones@redhat.com>
2012-03-31 11:44:00 +0100

    Rewrite virt-sysprep.

---
+++ sysprep/test-virt-sysprep.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -
+# libguestfs virt-sysprep test script
+# Copyright (C) 2011 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+export LANG=C
+set -e
+
+if [ ! -w /dev/fuse ]; then
+    echo "SKIPPING virt-sysprep test, because there is no /dev/fuse."
+    exit 0
+fi
+
+rm -f test.img
+
+qemu-img create -f qcow2 -o backing_file=../tests/guests/fedora.img test.img
+
+./virt-sysprep -a test.img
+
+rm -f test.img
