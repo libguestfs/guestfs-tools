libguestfs/guestfs-tools

commit 4354a3126152a2748cc9097cba139b3908ccc342

Add build system.
Partially copied from libguestfs repo.

builder/Makefile.am
@@ -174,14 +174,10 @@ virt_builder_repository_CFLAGS = \
 REPOSITORY_BOBJECTS = $(REPOSITORY_SOURCES_ML:.ml=.cmo)
 REPOSITORY_XOBJECTS = $(REPOSITORY_BOBJECTS:.cmo=.cmx)
 
-# -I $(top_builddir)/lib/.libs is a hack which forces corresponding -L
-# option to be passed to gcc, so we don't try linking against an
-# installed copy of libguestfs.
 OCAMLPACKAGES = \
-	-package str,unix \
+	-package str,unix,guestfs \
 	-I $(top_builddir)/common/utils/.libs \
 	-I $(top_builddir)/common/mlxml \
-	-I $(top_builddir)/lib/.libs \
 	-I $(top_builddir)/gnulib/lib/.libs \
 	-I $(top_builddir)/ocaml \
 	-I $(top_builddir)/common/mlstdutils \
@@ -363,11 +359,10 @@ TESTS = \
 	$(SLOW_TESTS)
 check_PROGRAMS =
 
-TESTS += test-virt-builder-list-simplestreams.sh
+TESTS += \
+	test-virt-builder-list-simplestreams.sh \
+	test-virt-builder.sh
 
-if ENABLE_APPLIANCE
-TESTS += test-virt-builder.sh
-endif ENABLE_APPLIANCE
 if HAVE_OCAML_PKG_OUNIT
 check_PROGRAMS += index_parser_tests
 TESTS += index_parser_tests

m4/guestfs-ocaml.m4
@@ -38,10 +38,10 @@ AC_ARG_ENABLE([ocaml],
     [],
     [enable_ocaml=yes])
 
-dnl OCaml >= 4.01 is required.
-ocaml_ver_str=4.01
+dnl OCaml >= 4.02 is required.
+ocaml_ver_str=4.02
 ocaml_min_major=4
-ocaml_min_minor=1
+ocaml_min_minor=2
 AC_MSG_CHECKING([if OCaml version >= $ocaml_ver_str])
 ocaml_major="`echo $OCAMLVERSION | $AWK -F. '{print $1}'`"
 ocaml_minor="`echo $OCAMLVERSION | $AWK -F. '{print $2}' | sed 's/^0//'`"
@@ -97,60 +97,12 @@ else
 fi
 AC_SUBST([OCAMLDEP_ONE_LINE])
 
-have_Hivex_OPEN_UNSAFE=no
-if test "x$enable_daemon" = "xyes"; then
-    OCAML_PKG_hivex=no
-    AC_CHECK_OCAML_PKG(hivex)
-    if test "x$OCAML_PKG_hivex" = "xno"; then
-        AC_MSG_ERROR([the OCaml module 'hivex' is required])
-    fi
-
-    # Check if Hivex has 'OPEN_UNSAFE' flag.
-    AC_MSG_CHECKING([for Hivex.OPEN_UNSAFE])
-    rm -f conftest.ml
-    echo 'let s = Hivex.OPEN_UNSAFE' > conftest.ml
-    if $OCAMLFIND ocamlc -package hivex -c conftest.ml >&5 2>&5 ; then
-        AC_MSG_RESULT([yes])
-        have_Hivex_OPEN_UNSAFE=yes
-    else
-        AC_MSG_RESULT([no])
-        have_Hivex_OPEN_UNSAFE=no
-    fi
-
-    dnl Check which OCaml runtime to link the daemon again.
-    dnl We can't use AC_CHECK_LIB here unfortunately because
-    dnl the other symbols are resolved by OCaml itself.
-    AC_MSG_CHECKING([which OCaml runtime we should link the daemon with])
-    if test "x$OCAMLOPT" != "xno"; then
-        for f in asmrun_pic asmrun; do
-            if test -f "$OCAMLLIB/lib$f.a"; then
-                CAMLRUN=$f
-                break
-            fi
-        done
-    else
-        for f in camlrun; do
-            if test -f "$OCAMLLIB/lib$f.a"; then
-                CAMLRUN=$f
-                break
-            fi
-        done
-    fi
-    if test "x$CAMLRUN" != "x"; then
-        AC_MSG_RESULT([$CAMLRUN])
-    else
-        AC_MSG_ERROR([could not find or link to libasmrun or libcamlrun])
-    fi
-    AC_SUBST([CAMLRUN])
+# Check for OCaml libguestfs bindings.
+OCAML_PKG_guestfs=no
+AC_CHECK_OCAML_PKG(guestfs)
+if test "x$OCAML_PKG_guestfs" = "xno"; then
+    AC_MSG_ERROR([the OCaml module 'guestfs' is required])
 fi
-
-dnl Define HIVEX_OPEN_UNSAFE_FLAG based on test above.
-AS_IF([test "x$have_Hivex_OPEN_UNSAFE" = "xno"],[
-    HIVEX_OPEN_UNSAFE_FLAG="None"
-],[
-    HIVEX_OPEN_UNSAFE_FLAG="Some Hivex.OPEN_UNSAFE"
-])
-AC_SUBST([HIVEX_OPEN_UNSAFE_FLAG])
 
 OCAML_PKG_gettext=no
 OCAML_PKG_oUnit=no
@@ -194,24 +146,6 @@ AM_CONDITIONAL([HAVE_OCAML_PKG_OUNIT],
 AC_CHECK_PROG([OCAML_GETTEXT],[ocaml-gettext],[ocaml-gettext],[no])
 AM_CONDITIONAL([HAVE_OCAML_GETTEXT],
                [test "x$OCAML_PKG_gettext" != "xno" && test "x$OCAML_GETTEXT" != "xno"])
-
-dnl Create the backwards compatibility Bytes module for OCaml < 4.02.
-mkdir -p common/mlstdutils
-rm -f common/mlstdutils/bytes.ml common/mlstdutils/bytes.mli
-AS_IF([test "x$have_Bytes_module" = "xno"],[
-    cat > common/mlstdutils/bytes.ml <<EOF
-include String
-let of_string = String.copy
-let to_string = String.copy
-let sub_string = String.sub
-EOF
-    $OCAMLC -i common/mlstdutils/bytes.ml > common/mlstdutils/bytes.mli
-    safe_string_option=
-],[
-    safe_string_option="-safe-string"
-])
-AM_CONDITIONAL([HAVE_BYTES_COMPAT_ML],
-	       [test "x$have_Bytes_module" = "xno"])
 
 dnl Check if OCaml has caml_alloc_initialized_string (added 2017).
 AS_IF([test "x$OCAMLC" != "xno" && test "x$OCAMLFIND" != "xno" && \

Makefile.am
@@ -1,0 +1,183 @@
+# guestfs tools
+# Copyright (C) 2009-2021 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+include $(top_srcdir)/common-rules.mk
+
+ACLOCAL_AMFLAGS = -I m4
+
+# Common code.
+SUBDIRS  = gnulib/lib
+SUBDIRS += common/utils
+SUBDIRS += common/structs
+SUBDIRS += common/mlstdutils
+SUBDIRS += common/mlutils
+SUBDIRS += common/mlpcre
+SUBDIRS += common/edit
+SUBDIRS += common/options
+SUBDIRS += common/parallel
+SUBDIRS += common/progress
+SUBDIRS += common/visit
+SUBDIRS += common/windows
+SUBDIRS += common/mlgettext
+SUBDIRS += common/mlprogress
+SUBDIRS += common/mlvisit
+SUBDIRS += common/mlxml
+SUBDIRS += common/mltools
+SUBDIRS += common/mlcustomize
+
+# Data used by the tests.
+SUBDIRS += test-data
+
+# virt-tools in C.
+SUBDIRS += align cat diff df edit format inspector make-fs
+
+# OCaml tools.  Note 'common/ml*' and 'customize' contain shared code
+# used by other OCaml tools, so these must come first.
+SUBDIRS += customize
+SUBDIRS += builder builder/templates
+SUBDIRS += get-kernel
+SUBDIRS += resize
+SUBDIRS += sparsify
+SUBDIRS += sysprep
+SUBDIRS += dib
+
+# bash-completion
+SUBDIRS += bash
+
+# Perl tools.
+SUBDIRS += win-reg
+
+# After all source files were used we can generate the translation strings
+SUBDIRS += po
+if HAVE_PO4A
+SUBDIRS += po-docs
+endif
+
+EXTRA_DIST = \
+	README \
+	.gitignore \
+	check-mli.sh \
+	common/.gitignore \
+	common/README \
+	lib/guestfs-internal-all.h \
+	m4/.gitignore \
+	m4/introspection.m4 \
+	podcheck.pl \
+	test-functions.sh \
+	tmp/.gitignore \
+	valgrind-suppressions
+
+# When doing 'make dist' update a few files automatically.
+#
+#  po/POTFILES    - files with ordinary extensions, but not OCaml files
+#  po/POTFILES-pl - Perl files that don't end in *.pl, which need a
+#                   special xgettext option [not generated here]
+#  po/POTFILES-ml - OCaml files, which need a special tool to translate
+
+dist-hook: po/POTFILES po/POTFILES-ml
+
+# For more information about translations, see po/Makefile.am.
+po/POTFILES: configure.ac
+	rm -f $@ $@-t
+	cd $(srcdir); \
+	find $(DIST_SUBDIRS) -name '*.c' | \
+	grep -v -E '^(examples|gnulib|perl/(blib|examples)|po-docs|tests|test-data|bundled)/' | \
+	grep -v -E '/((guestfs|rc)_protocol\.c|dummy\.c)$$' | \
+	grep -v -E '^python/utils\.c$$' | \
+	grep -v -E '^perl/lib/Sys/Guestfs\.c$$' | \
+	grep -v -E '^common/errnostring' | \
+	grep -v -E '.*-(tests|gperf)\.c$$' | \
+	LC_ALL=C sort -u > $@-t
+	mv $@-t $@
+
+po/POTFILES-ml: configure.ac
+	rm -f $@ $@-t
+	cd $(srcdir); \
+	find builder common/ml* customize dib get-kernel resize sparsify sysprep -name '*.ml' | \
+	grep -v '^builder/templates/' | \
+	grep -v '^common/mlv2v/' | \
+	grep -v -E '.*_tests\.ml$$' | \
+	LC_ALL=C sort > $@-t
+	mv $@-t $@
+
+# NB. podwrapper is an internal tool, so the man page mustn't be installed.
+# It should be noinst_MANS but that doesn't work.
+noinst_DATA = podwrapper.1
+podwrapper.1: podwrapper.pl
+	$(PODWRAPPER) \
+	  --section 1 \
+	  --man $@-t \
+	  --license GPLv2+ \
+	  --warning safe \
+	  $<
+	mv $@-t $@
+
+# Make clean.
+CLEANFILES += \
+	pod2htm?.tmp
+
+clean-local:
+	-find . -name '*~' -delete
+
+build-test-guests:
+	$(MAKE) -C test-data/phony-guests check
+
+# Some tests which run at the top level.
+TESTS = check-mli.sh
+
+# Commit everything in current directory to HEAD, and set commit
+# message to current version (only for maintainer).
+
+maintainer-commit:
+	git commit -a -m "Version $(VERSION)."
+
+# Tag HEAD with current version (only for maintainer).
+
+maintainer-tag:
+	git tag -a "v$(VERSION)" -m "Version $(VERSION)" -f
+
+# Maintainer only: check no files are missing from EXTRA_DIST rules,
+# and that all generated files have been included in the tarball.
+# (Note you must have done 'make dist')
+
+maintainer-check-extra-dist:
+	zcat $(PACKAGE_NAME)-$(VERSION).tar.gz | tar tf - | sort | \
+	  sed 's,^$(PACKAGE_NAME)-$(VERSION)/,,' > tmp/tarfiles
+	( git ls-files ; \
+	  cd common; git ls-files | sed 's,^,common/,' ) | \
+	  grep -v '^common$$' | \
+	  grep -v '^common/errnostring/' | \
+	  grep -v '^common/mlv2v/' | \
+	  grep -v '^common/protocol/' | \
+	  grep -v '^common/qemuopts/' | \
+	  grep -v '^intltool-.*\.in' | \
+	  grep -v '^\.gitmodules' | \
+	  grep -v '^\.gnulib' | \
+	  sort > tmp/gitfiles
+	comm -13 tmp/tarfiles tmp/gitfiles > tmp/comm-out
+	@echo Checking for differences between EXTRA_DIST and git ...
+	cat tmp/comm-out
+	[ ! -s tmp/comm-out ]
+	@echo Checking for generated files missing from the tarball ...
+	@for f in `cat generator/files-generated.txt | grep -v '^common/mlv2v/'`; do \
+	  if ! grep -sq "^$$f\$$" tmp/tarfiles; then \
+	    echo generated file missing from tarball: $$f; \
+	    exit 1; \
+	  fi; \
+	done
+	rm tmp/tarfiles tmp/gitfiles tmp/comm-out
+	@echo PASS: EXTRA_DIST tests

align/Makefile.am
@@ -42,6 +42,7 @@ virt_alignment_scan_CPPFLAGS = \
 virt_alignment_scan_CFLAGS = \
 	-pthread \
 	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
+	$(LIBGUESTFS_CFLAGS) \
 	$(LIBXML2_CFLAGS) \
 	$(LIBVIRT_CFLAGS)
 
@@ -50,7 +51,7 @@ virt_alignment_scan_LDADD = \
 	$(top_builddir)/common/parallel/libparallel.la \
 	$(top_builddir)/common/structs/libstructs.la \
 	$(top_builddir)/common/utils/libutils.la \
-	$(top_builddir)/lib/libguestfs.la \
+	$(LIBGUESTFS_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(LIBVIRT_LIBS) \
 	$(LTLIBINTL) \
@@ -78,16 +79,12 @@ stamp-virt-alignment-scan.pod: virt-alignment-scan.pod
 TESTS_ENVIRONMENT = $(top_builddir)/run --test
 
 TESTS = \
-	test-virt-alignment-scan-docs.sh
-
-if ENABLE_APPLIANCE
-TESTS += \
+	test-virt-alignment-scan-docs.sh \
 	test-virt-alignment-scan.sh
 
 if HAVE_LIBVIRT
 TESTS += \
 	test-virt-alignment-scan-guests.sh
-endif
 endif
 
 check-valgrind:

bash/Makefile.am
@@ -18,19 +18,13 @@
 include $(top_srcdir)/subdir-rules.mk
 
 scripts = \
-	guestfish \
-	guestmount \
-	libguestfs-test-tool \
 	virt-alignment-scan \
-	virt-rescue
+	virt-win-reg
 
 # Some of the scripts are simply symbolic links.
 symlinks = \
-	guestunmount \
 	virt-builder \
 	virt-cat \
-	virt-copy-in \
-	virt-copy-out \
 	virt-customize \
 	virt-diff \
 	virt-df \
@@ -45,10 +39,7 @@ symlinks = \
 	virt-resize \
 	virt-sparsify \
 	virt-sysprep \
-	virt-tail \
-	virt-tar-in \
-	virt-tar-out \
-	virt-win-reg
+	virt-tail
 
 # Note: Don't distribute the symbolic links, only the real files.
 EXTRA_DIST = \
@@ -59,18 +50,10 @@ EXTRA_DIST = \
 CLEANFILES += \
 	$(symlinks)
 
-# Any tool that has --short-options and --long-options only is handled
-# by this common script.
-virt-win-reg:
-	rm -f $@
-	$(LN_S) libguestfs-test-tool $@
-
 # Any tool that has --short-options and --long-options and a few
 # common options like -d is handled by this common script.  However
 # this script cannot deal with commands that use --ro/--rw
-# (eg. virt-rescue), nor commands that have lots of exceptions
-# (eg. guestfish).  Those tools have to be handled individually.
-guestunmount \
+# (eg. virt-rescue).  Those tools have to be handled individually.
 virt-builder virt-cat virt-customize virt-df virt-dib virt-diff \
 virt-edit virt-filesystems virt-format virt-get-kernel virt-inspector \
 virt-log virt-ls \
@@ -78,11 +61,6 @@ virt-resize virt-sparsify virt-sysprep \
 virt-tail:
 	rm -f $@
 	$(LN_S) virt-alignment-scan $@
-
-# guestfish is handled specially.
-virt-copy-in virt-copy-out virt-tar-in virt-tar-out:
-	rm -f $@
-	$(LN_S) guestfish $@
 
 if HAVE_BASH_COMPLETION
 

cat/Makefile.am
@@ -48,14 +48,15 @@ virt_cat_CPPFLAGS = \
 
 virt_cat_CFLAGS = \
 	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
-	$(LIBXML2_CFLAGS)
+	$(LIBXML2_CFLAGS) \
+	$(LIBGUESTFS_CFLAGS)
 
 virt_cat_LDADD = \
 	$(top_builddir)/common/options/liboptions.la \
 	$(top_builddir)/common/windows/libwindows.la \
 	$(top_builddir)/common/structs/libstructs.la \
 	$(top_builddir)/common/utils/libutils.la \
-	$(top_builddir)/lib/libguestfs.la \
+	$(LIBGUESTFS_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(LIBVIRT_LIBS) \
 	$(LTLIBINTL) \
@@ -77,14 +78,15 @@ virt_filesystems_CPPFLAGS = \
 
 virt_filesystems_CFLAGS = \
 	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
-	$(LIBXML2_CFLAGS)
+	$(LIBXML2_CFLAGS) \
+	$(LIBGUESTFS_CFLAGS)
 
 virt_filesystems_LDADD = \
 	$(top_builddir)/common/options/liboptions.la \
 	$(top_builddir)/common/windows/libwindows.la \
 	$(top_builddir)/common/structs/libstructs.la \
 	$(top_builddir)/common/utils/libutils.la \
-	$(top_builddir)/lib/libguestfs.la \
+	$(LIBGUESTFS_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(LIBVIRT_LIBS) \
 	$(LTLIBINTL) \
@@ -106,13 +108,14 @@ virt_log_CPPFLAGS = \
 
 virt_log_CFLAGS = \
 	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
-	$(LIBXML2_CFLAGS)
+	$(LIBXML2_CFLAGS) \
+	$(LIBGUESTFS_CFLAGS)
 
 virt_log_LDADD = \
 	$(top_builddir)/common/options/liboptions.la \
 	$(top_builddir)/common/structs/libstructs.la \
 	$(top_builddir)/common/utils/libutils.la \
-	$(top_builddir)/lib/libguestfs.la \
+	$(LIBGUESTFS_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(LIBVIRT_LIBS) \
 	$(LTLIBINTL) \
@@ -135,14 +138,15 @@ virt_ls_CPPFLAGS = \
 
 virt_ls_CFLAGS = \
 	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
-	$(LIBXML2_CFLAGS)
+	$(LIBXML2_CFLAGS) \
+	$(LIBGUESTFS_LIBS)
 
 virt_ls_LDADD = \
 	$(top_builddir)/common/options/liboptions.la \
 	$(top_builddir)/common/visit/libvisit.la \
 	$(top_builddir)/common/structs/libstructs.la \
 	$(top_builddir)/common/utils/libutils.la \
-	$(top_builddir)/lib/libguestfs.la \
+	$(LIBGUESTFS_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(LIBVIRT_LIBS) \
 	$(LTLIBINTL) \
@@ -164,14 +168,15 @@ virt_tail_CPPFLAGS = \
 
 virt_tail_CFLAGS = \
 	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
-	$(LIBXML2_CFLAGS)
+	$(LIBXML2_CFLAGS) \
+	$(LIBGUESTFS_CFLAGS)
 
 virt_tail_LDADD = \
 	$(top_builddir)/common/options/liboptions.la \
 	$(top_builddir)/common/windows/libwindows.la \
 	$(top_builddir)/common/structs/libstructs.la \
 	$(top_builddir)/common/utils/libutils.la \
-	$(top_builddir)/lib/libguestfs.la \
+	$(LIBGUESTFS_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(LIBVIRT_LIBS) \
 	$(LTLIBINTL) \
@@ -252,16 +257,12 @@ stamp-virt-tail.pod: virt-tail.pod
 TESTS_ENVIRONMENT = $(top_builddir)/run --test
 
 TESTS = \
-	test-docs.sh
-
-if ENABLE_APPLIANCE
-TESTS += \
+	test-docs.sh \
 	test-virt-cat.sh \
 	test-virt-filesystems.sh \
 	test-virt-log.sh \
 	test-virt-ls.sh \
 	test-virt-tail.sh
-endif ENABLE_APPLIANCE
 
 check-valgrind:
 	$(MAKE) VG="@VG@" check

customize/Makefile.am
@@ -117,13 +117,9 @@ else
 OBJECTS = $(XOBJECTS)
 endif
 
-# -I $(top_builddir)/lib/.libs is a hack which forces corresponding -L
-# option to be passed to gcc, so we don't try linking against an
-# installed copy of libguestfs.
 OCAMLPACKAGES = \
-	-package str,unix \
+	-package str,unix,guestfs \
 	-I $(top_builddir)/common/utils/.libs \
-	-I $(top_builddir)/lib/.libs \
 	-I $(top_builddir)/gnulib/lib/.libs \
 	-I $(top_builddir)/ocaml \
 	-I $(top_builddir)/common/mlstdutils \
@@ -218,13 +214,9 @@ stamp-virt-customize.pod: virt-customize.pod $(top_srcdir)/common/mlcustomize/cu
 TESTS_ENVIRONMENT = $(top_builddir)/run --test
 
 TESTS = \
-	test-virt-customize-docs.sh
-
-if ENABLE_APPLIANCE
-TESTS += \
+	test-virt-customize-docs.sh \
 	test-virt-customize.sh \
 	$(SLOW_TESTS)
-endif
 
 check-valgrind:
 	$(MAKE) VG="@VG@" check

df/Makefile.am
@@ -46,6 +46,7 @@ virt_df_CPPFLAGS = \
 virt_df_CFLAGS = \
 	-pthread \
 	$(WARN_CFLAGS) $(WERROR_CFLAGS) \
+	$(LIBGUESTFS_CFLAGS) \
 	$(LIBXML2_CFLAGS) \
 	$(LIBVIRT_CFLAGS)
 
@@ -54,7 +55,7 @@ virt_df_LDADD = \
 	$(top_builddir)/common/parallel/libparallel.la \
 	$(top_builddir)/common/structs/libstructs.la \
 	$(top_builddir)/common/utils/libutils.la \
-	$(top_builddir)/lib/libguestfs.la \
+	$(LIBGUESTFS_LIBS) \
 	$(LIBXML2_LIBS) \
 	$(LIBVIRT_LIBS) \
 	$(LTLIBINTL) \
@@ -82,17 +83,13 @@ stamp-virt-df.pod: virt-df.pod
 TESTS_ENVIRONMENT = $(top_builddir)/run --test
 
 TESTS = \
-	test-virt-df-docs.sh
-
-if ENABLE_APPLIANCE
-TESTS += \
+	test-virt-df-docs.sh \
 	test-virt-df.sh
 
 if HAVE_LIBVIRT
 TESTS += \
 	test-virt-df-guests.sh
 endif
-endif ENABLE_APPLIANCE
 
 check-valgrind:
 	$(MAKE) VG="@VG@" check

commit b470270d44e8dc37a2417c1157eefdd9c3d65dd1

builder: Use correct ounit2 module.
Fixes: commit 4354a3126152a2748cc9097cba139b3908ccc342

builder/Makefile.am
@@ -192,7 +192,7 @@ if HAVE_OCAML_PKG_GETTEXT
 OCAMLPACKAGES += -package gettext-stub
 endif
 if HAVE_OCAML_PKG_OUNIT
-OCAMLPACKAGES_TESTS += -package oUnit
+OCAMLPACKAGES_TESTS += -package ounit2
 endif
 
 OCAMLCLIBS = \

m4/guestfs-ocaml.m4
@@ -105,7 +105,7 @@ if test "x$OCAML_PKG_guestfs" = "xno"; then
 fi
 
 OCAML_PKG_gettext=no
-OCAML_PKG_oUnit=no
+OCAML_PKG_ounit2=no
 ounit_is_v2=no
 have_Bytes_module=no
 AS_IF([test "x$OCAMLC" != "xno"],[
@@ -117,11 +117,11 @@ AS_IF([test "x$OCAMLC" != "xno"],[
 
     GUESTFS_CREATE_COMMON_GETTEXT_ML([common/mlgettext/common_gettext.ml])
 
-    AC_CHECK_OCAML_PKG(oUnit)
+    AC_CHECK_OCAML_PKG(ounit2)
 
     # oUnit >= 2 is required, so check that it has OUnit2.
-    if test "x$OCAML_PKG_oUnit" != "xno"; then
-        AC_CHECK_OCAML_MODULE(ounit_is_v2,[OUnit.OUnit2],OUnit2,[+oUnit])
+    if test "x$OCAML_PKG_ounit2" != "xno"; then
+        AC_CHECK_OCAML_MODULE(ounit_is_v2,[OUnit.OUnit2],OUnit2,[+ounit2])
     fi
 
     # Check if we have the 'Bytes' module.  If not (OCaml < 4.02) then
@@ -141,7 +141,7 @@ AS_IF([test "x$OCAMLC" != "xno"],[
 AM_CONDITIONAL([HAVE_OCAML_PKG_GETTEXT],
                [test "x$OCAML_PKG_gettext" != "xno"])
 AM_CONDITIONAL([HAVE_OCAML_PKG_OUNIT],
-               [test "x$OCAML_PKG_oUnit" != "xno" && test "x$ounit_is_v2" != "xno"])
+               [test "x$OCAML_PKG_ounit2" != "xno" && test "x$ounit_is_v2" != "xno"])
 
 AC_CHECK_PROG([OCAML_GETTEXT],[ocaml-gettext],[ocaml-gettext],[no])
 AM_CONDITIONAL([HAVE_OCAML_GETTEXT],