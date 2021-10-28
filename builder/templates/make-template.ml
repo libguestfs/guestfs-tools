024872fe462dd22b843be7875b7423564016aa7f
Lars Kaiser <lars@kaiser.yt>
2021-10-28 20:20:50 +0200

    builder: templates: Add Debian 11 (bullseye)
    From https://github.com/rwmjones/guestfs-tools/pull/5

--- builder/templates/make-template.ml
+++ builder/templates/make-template.ml
@@ -357,6 +357,7 @@ and os_of_string os ver =
   | "debian", "8" -> Debian (8, "jessie")
   | "debian", "9" -> Debian (9, "stretch")
   | "debian", "10" -> Debian (10, "buster")
+  | "debian", "11" -> Debian (11, "bullseye")
   | "ubuntu", "10.04" -> Ubuntu (ver, "lucid")
   | "ubuntu", "12.04" -> Ubuntu (ver, "precise")
   | "ubuntu", "14.04" -> Ubuntu (ver, "trusty")
