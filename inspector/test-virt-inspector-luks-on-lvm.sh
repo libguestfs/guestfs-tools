27da4b0c4991f8de44ea879afdc5930cdbe002c9
Laszlo Ersek <lersek@redhat.com>
2022-02-23 17:22:15 +0100

    inspector: add LUKS-on-LVM test
    Port libguestfs patch 'tests: add LUKS-on-LVM test' (commit 322113314055)
    to virt-inspector.
    
    (This patch is best formatted with "--find-copies-harder".)
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1658126
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20220223162215.16782-4-lersek@redhat.com>
    Acked-by: Richard W.M. Jones <rjones@redhat.com>
    [lersek@redhat.com: resolve libguestfs patch title to commit hash]

---
+++ inspector/test-virt-inspector-luks-on-lvm.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -
+# libguestfs virt-inspector test script
+# Copyright (C) 2012-2020 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Test that virt-inspector can work on encrypted images when the
+# right password is supplied.
+#
+# Regression test for https://bugzilla.redhat.com/show_bug.cgi?id=1658126
+
+set -e
+set -x
+
+$TEST_FUNCTIONS
+skip_if_skipped
+
+f=../test-data/phony-guests/fedora-luks-on-lvm.img
+keys=(--key /dev/VG/Root:key:FEDORA-Root
+      --key /dev/VG/LV1:key:FEDORA-LV1
+      --key /dev/VG/LV2:key:FEDORA-LV2
+      --key /dev/VG/LV3:key:FEDORA-LV3)
+
+# Ignore zero-sized file.
+if [ -s "$f" ]; then
+    uuid_root=$(guestfish --ro -i -a "$f" "${keys[@]}" luks-uuid /dev/VG/Root)
+    b=$(basename "$f")
+    $VG virt-inspector "${keys[@]}" --format=raw -a "$f" > "actual-$b.xml"
+    # Check the generated output validate the schema.
+    $XMLLINT --noout --relaxng "$srcdir/virt-inspector.rng" "actual-$b.xml"
+    # This 'diff' command will fail (because of -e option) if there
+    # are any differences.
+    sed -e "s/ROOTUUID/$uuid_root/" < "$srcdir/expected-$b.xml" \
+    | diff -u - "actual-$b.xml"
+fi
