libguestfs/guestfs-tools

commit 3fe941767042bf83d9a252b0819fa6d5a48059d0

builder: Fix Fedora 33 boot problem.
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1911177
Thanks: Daniel P. Berrangé

(cherry picked from commit 0c829875310aca5cb3d85b344e798682c37290d5)

builder/templates/make-template.ml
@@ -622,7 +622,9 @@ dracut -f /boot/initramfs-$KERNEL_VERSION.img $KERNEL_VERSION
       bpf "%%post\n";
       bpf "\
 # Ensure the installation is up-to-date.
-dnf -y --best upgrade
+# This makes Fedora >= 33 unbootable, see:
+# https://bugzilla.redhat.com/show_bug.cgi?id=1911177
+#dnf -y --best upgrade
 ";
 
       let needs_regenerate_dracut = ref false in

builder/templates/fedora-33.index-fragment
@@ -3,10 +3,11 @@ name=Fedora® 33 Server
 osinfo=fedora33
 arch=x86_64
 file=fedora-33.xz
-checksum[sha512]=7ef3a333456ee4deda38c369616a4423652029837f7aa09862940327608d285c79e1c7ed90c3bb4814878dbdce3fa1f11a8625439739743bce73947fc80c08eb
+revision=2
+checksum[sha512]=f80a018b494d5f2e44873094855f03a1f15080ee46f88d825bfe1de55b5903f5d9d822de18122ecac00d6295b2a8e7461950befef4c494ebcfe6b1f5e3aa5cc3
 format=raw
 size=6442450944
-compressed_size=501145952
+compressed_size=392812568
 expand=/dev/sda3
 notes=Fedora® 33 Server
  

builder/templates/fedora-33.ks
@@ -28,7 +28,7 @@ poweroff
 
 %post
 # Ensure the installation is up-to-date.
-dnf -y --best upgrade
+#dnf -y --best upgrade
 # Enable Xen domU support.
 pushd /etc/dracut.conf.d
 echo 'add_drivers+=" xen:vbd xen:vif "' > virt-builder-xen-drivers.conf

builder/templates/fedora-33.virt-install-cmd
@@ -5,7 +5,7 @@
 
 'virt-install' \
     '--transient' \
-    '--name=tmp-hosoapgs' \
+    '--name=tmp-vxsw7p2j' \
     '--ram=4096' \
     '--arch=x86_64' \
     '--cpu=host' \
@@ -13,7 +13,7 @@
     '--os-variant=fedora26' \
     '--initrd-inject=fedora-33.ks' \
     '--extra-args=ks=file:/fedora-33.ks  console=tty0 console=ttyS0,115200 rd_NO_PLYMOUTH' \
-    '--disk=/home/rjones/d/libguestfs/builder/templates/tmp-hosoapgs.img,size=6,format=raw' \
+    '--disk=/home/rjones/d/libguestfs/builder/templates/tmp-vxsw7p2j.img,size=6,format=raw' \
     '--location=http://mirror.bytemark.co.uk/fedora/linux/releases/33/Server/x86_64/os/' \
     '--serial=pty' \
     '--nographics' 

commit f2c0bf5387d094900de6420cb6502aa00d9c9b24

builder: templates: Enable Fedora image update again
Thanks: fepitre on IRC
Fixes: commit 3fe941767042bf83d9a252b0819fa6d5a48059d0

builder/templates/fedora-35.index-fragment
@@ -3,10 +3,11 @@ name=Fedora® 35 Server
 osinfo=fedora35
 arch=x86_64
 file=fedora-35.xz
-checksum[sha512]=bd5d6c1311463b7bee3c3743ab14ad05dfbfedc135933c519015e62ebd127d904be7222391fb73c4631044e44b5f70038a28d87b5a6be1508bca752517929415
+revision=2
+checksum[sha512]=fa4ce7f9d37c76dcf1ed3d30668227f738761d97503daf4a47565b1d14de4e6d577855886b3e2e55429163753af84a5da1a58aa5aa975e6f189542b4f9afac61
 format=raw
 size=6442450944
-compressed_size=474387772
+compressed_size=623046248
 expand=/dev/sda3
 notes=Fedora® 35 Server
  

builder/templates/fedora-35.ks
@@ -27,9 +27,11 @@ poweroff
 
 %post
 # Ensure the installation is up-to-date.
-# This makes Fedora >= 33 unbootable, see:
+dnf -y --best upgrade
+# This required otherwise the kernel will not be bootable, see
 # https://bugzilla.redhat.com/show_bug.cgi?id=1911177
-#dnf -y --best upgrade
+# https://bugzilla.redhat.com/show_bug.cgi?id=1945835#c24
+grub2-mkconfig -o /boot/grub2/grub.cfg
 # Enable Xen domU support.
 pushd /etc/dracut.conf.d
 echo 'add_drivers+=" xen:vbd xen:vif "' > virt-builder-xen-drivers.conf

builder/templates/fedora-35.virt-install-cmd
@@ -5,7 +5,7 @@
 
 'virt-install' \
     '--transient' \
-    '--name=tmp-4qrk2p3m' \
+    '--name=tmp-tn6upkps' \
     '--ram=4096' \
     '--arch=x86_64' \
     '--cpu=host' \
@@ -13,7 +13,7 @@
     '--os-variant=fedora34' \
     '--initrd-inject=fedora-35.ks' \
     '--extra-args=inst.ks=file:/fedora-35.ks  console=tty0 console=ttyS0,115200 rd_NO_PLYMOUTH' \
-    '--disk=/home/rjones/d/guestfs-tools/builder/templates/tmp-4qrk2p3m.img,size=6,format=raw' \
+    '--disk=/home/rjones/d/guestfs-tools/builder/templates/tmp-tn6upkps.img,size=6,format=raw' \
     '--location=https://mirror.bytemark.co.uk/fedora/linux/releases/35/Server/x86_64/os/' \
     '--serial=pty' \
     '--nographics' 

builder/templates/make-template.ml
@@ -649,9 +649,11 @@ dracut -f /boot/initramfs-$KERNEL_VERSION.img $KERNEL_VERSION
       bpf "%%post\n";
       bpf "\
 # Ensure the installation is up-to-date.
-# This makes Fedora >= 33 unbootable, see:
+dnf -y --best upgrade
+# This required otherwise the kernel will not be bootable, see
 # https://bugzilla.redhat.com/show_bug.cgi?id=1911177
-#dnf -y --best upgrade
+# https://bugzilla.redhat.com/show_bug.cgi?id=1945835#c24
+grub2-mkconfig -o /boot/grub2/grub.cfg
 ";
 
       let needs_regenerate_dracut = ref false in