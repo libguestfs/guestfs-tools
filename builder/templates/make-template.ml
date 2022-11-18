d3c0e9205f332217dccdd7020234668d4a445617
Richard W.M. Jones <rjones@redhat.com>
2022-11-18 09:13:42 +0000

    builder: templates: Add rhel-9.1
    Red Hat internal image changed directory again.

--- builder/templates/make-template.ml
+++ builder/templates/make-template.ml
@@ -952,7 +952,7 @@ and make_boot_media os arch =
      Location (sprintf "http://download.eng.bos.redhat.com/released/rhel-6-7-8/rhel-8/RHEL-8/8.%d.0/BaseOS/%s/os" minor (string_of_arch arch))
 
   | RHEL (9, minor), arch ->
-     Location (sprintf "http://download.eng.bos.redhat.com/released/rhel-9/RHEL-9/9.%d.0/BaseOS/%s/os" minor (string_of_arch arch))
+     Location (sprintf "http://download.eng.bos.redhat.com/released/RHEL-9/9.%d.0/BaseOS/%s/os" minor (string_of_arch arch))
 
   | Ubuntu (_, dist), X86_64 ->
      Location (sprintf "http://archive.ubuntu.com/ubuntu/dists/%s/main/installer-amd64" dist)
