libguestfs/guestfs-tools

commit d42aa6d273879d3f0b0039139775b882517073ac

customize: Add --ssh-inject option for injecting SSH keys.
This adds a customize option:

  virt-customize --ssh-inject USER
  virt-customize --ssh-inject USER:string:KEY_STRING
  virt-customize --ssh-inject USER:file:FILENAME

(ditto for virt-builder and virt-sysprep)

In each case this injects into the guest user USER
a) the current (host) user's ssh pubkey
b) the key specified as KEY_STRING
c) the key in FILENAME
adding it to ~USER/.ssh/authorized_keys in the guest.

For example:

  virt-builder fedora-20 --ssh-inject root

will add the local user's ssh pubkey into the root account of the
newly created guest.  Or:

  virt-customize -a disk.img \
     --ssh-inject 'mary:string:ssh-rsa AAAA.... mary@localhost'

adds the given ssh pubkey to mary's account in the guest.

This doesn't set the SELinux labels correctly on newly created files
and directories, so you have to use --selinux-relabel (probably we
should fix this as part of the general effort to fix SELinux
relabelling).  However it should preserve the labels if the
~/.ssh/authorized_keys file already exists.

Most of this work is based on a patch sent to the mailing list by
Richard W.M. Jones <rjones@redhat.com>:
https://www.redhat.com/archives/libguestfs/2014-November/msg00000.html

customize/customize_run.ml
@@ -232,6 +232,14 @@ exec >>%s 2>&1
       msg (f_"Scrubbing: %s") path;
       g#scrub_file path
 
+    | `SSHInject (user, selector) ->
+      (match g#inspect_get_type root with
+      | "linux" | "freebsd" | "netbsd" | "openbsd" | "hurd" ->
+        msg (f_"SSH key inject: %s") user;
+        Ssh_key.do_ssh_inject_unix g user selector
+      | _ ->
+        warning (f_"SSH key could be injected for this type of guest"))
+
     | `Timezone tz ->
       msg (f_"Setting the timezone: %s") tz;
       if not (Timezone.set_timezone g root tz) then

builder/Makefile.am
@@ -111,6 +111,7 @@ deps = \
 	$(top_builddir)/customize/crypt-c.o \
 	$(top_builddir)/customize/crypt.cmx \
 	$(top_builddir)/customize/password.cmx \
+	$(top_builddir)/customize/ssh_key.cmx \
 	$(top_builddir)/customize/customize_cmdline.cmx \
 	$(top_builddir)/customize/customize_run.cmx \
 	$(top_builddir)/fish/guestfish-uri.o \

builder/cmdline.ml
@@ -306,8 +306,8 @@ read the man page virt-builder(1).
           | `Command _ | `InstallPackages _ | `Script _ | `Update -> true
           | `Delete _ | `Edit _ | `FirstbootCommand _ | `FirstbootPackages _
           | `FirstbootScript _ | `Hostname _ | `Link _ | `Mkdir _
-          | `Password _ | `RootPassword _ | `Scrub _ | `Timezone _ | `Upload _
-          | `Write _ | `Chmod _ -> false
+          | `Password _ | `RootPassword _ | `Scrub _ | `SSHInject _
+          | `Timezone _ | `Upload _ | `Write _ | `Chmod _ -> false
         ) ops.ops in
         if requires_execute_on_guest then
           error (f_"sorry, cannot run commands on a guest with a different architecture");

builder/virt-builder.pod
@@ -774,6 +774,37 @@ If C</tmp> or C<C:\Temp> is missing.
 If you don't want the log file to appear in the final image, then
 use the I<--no-logfile> command line option.
 
+=head2 SSH KEYS
+
+The I<--ssh-inject> option is used to inject ssh keys for users in
+the guest, so they can login without supplying a password.
+
+The C<SELECTOR> part of the option value is optional; in this case,
+I<--ssh-inject> C<USER> means that we look in the I<current>
+user's C<~/.ssh> directory to find the default public ID file.  That
+key is uploaded.  "default public ID" is the I<default_ID_file> file
+described in L<ssh-copy-id(1)>.
+
+If specified, the C<SELECTOR> can be in one of the following formats:
+
+=over 4
+
+=item B<--ssh-inject> USER:file:FILENAME
+
+Read the ssh key from C<FILENAME>.  C<FILENAME> is usually a I<.pub>
+file.
+
+=item B<--ssh-inject> USER:string:KEY_STRING
+
+Use the specified C<KEY_STRING>.  C<KEY_STRING> is usually a public
+string like I<ssh-rsa AAAA.... user@localhost>.
+
+=back
+
+In any case, the C<~USER/.ssh> directory and the
+C<~USER/.ssh/authorized_keys> file will be created if not existing
+already.
+
 =head2 INSTALLATION PROCESS
 
 When you invoke virt-builder, installation proceeds as follows:

customize/Makefile.am
@@ -55,6 +55,8 @@ SOURCES = \
 	perl_edit.mli \
 	random_seed.ml \
 	random_seed.mli \
+	ssh_key.ml \
+	ssh_key.mli \
 	timezone.ml \
 	timezone.mli \
 	urandom.ml \
@@ -92,6 +94,7 @@ ocaml_modules = \
 	password \
 	perl_edit \
 	random_seed \
+	ssh_key \
 	timezone \
 	customize_cmdline \
 	customize_run \

customize/ssh_key.ml
@@ -1,0 +1,133 @@
+(* virt-customize
+ * Copyright (C) 2014 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Common_gettext.Gettext
+open Common_utils
+
+open Customize_utils
+
+open Printf
+open Sys
+open Unix
+
+module G = Guestfs
+
+type ssh_key_selector =
+| SystemKey
+| KeyFile of string
+| KeyString of string
+
+let rec parse_selector arg =
+  parse_selector_list arg (string_nsplit ":" arg)
+
+and parse_selector_list orig_arg = function
+  | [] | [ "" ] ->
+    SystemKey
+  | [ "file"; f ] ->
+    KeyFile f
+  | [ "string"; s ] ->
+    KeyString s
+  | _ ->
+    error (f_"invalid ssh-inject selector '%s'; see the man page") orig_arg
+
+(* Find the local [on the host] user's SSH public key.  See
+ * ssh-copy-id(1) default_ID_file for rationale.
+ *)
+let pubkey_re = Str.regexp "^id.*\\.pub$"
+let pubkey_ignore_re = Str.regexp ".*-cert\\.pub$"
+
+let local_user_ssh_pubkey () =
+  let home_dir =
+    try getenv "HOME"
+    with Not_found ->
+      error (f_"ssh-inject: $HOME environment variable is not set") in
+  let ssh_dir = home_dir // ".ssh" in
+  let files = Sys.readdir ssh_dir in
+  let files = Array.to_list files in
+  let files = List.filter (
+    fun file ->
+      Str.string_match pubkey_re file 0 &&
+        not (Str.string_match pubkey_ignore_re file 0)
+  ) files in
+  if files = [] then
+    error (f_"ssh-inject: no public key file found in %s") ssh_dir;
+
+  (* Newest file. *)
+  let files = List.map (
+    fun file ->
+      let file = ssh_dir // file in
+      let stat = stat file in
+      (file, stat.st_mtime)
+  ) files in
+  let files = List.sort (fun (_,m1) (_,m2) -> compare m2 m1) files in
+
+  fst (List.hd files)
+
+let read_key file =
+  (* Read and return the public key. *)
+  let key = read_whole_file file in
+  if key = "" then
+    error (f_"ssh-inject: public key file (%s) is empty") file;
+  key
+
+let key_string_from_selector = function
+  | SystemKey ->
+    read_key (local_user_ssh_pubkey ())
+  | KeyFile f ->
+    read_key f
+  | KeyString s ->
+    if String.length s < 1 then
+      error (f_"ssh-inject: key is an empty string");
+    s
+
+(* Inject SSH key, where possible. *)
+let do_ssh_inject_unix (g : Guestfs.guestfs) user selector =
+  let key = key_string_from_selector selector in
+  assert (String.length key > 0);
+
+  (* If the key doesn't have \n at the end, add it. *)
+  let len = String.length key in
+  let key = if key.[len-1] = '\n' then key else key ^ "\n" in
+
+  (* Get user's home directory. *)
+  g#aug_init "/" 0;
+  let home_dir =
+    try
+      let expr = sprintf "/files/etc/passwd/%s/home" user in
+      g#aug_get expr
+    with G.Error _ ->
+      error (f_"ssh-inject: the user %s does not exist on the guest")
+        user in
+  g#aug_close ();
+
+  (* Create ~user/.ssh if it doesn't exist. *)
+  let ssh_dir = sprintf "%s/.ssh" home_dir in
+  if not (g#exists ssh_dir) then (
+    g#mkdir ssh_dir;
+    g#chmod 0o755 ssh_dir
+  );
+
+  (* Create ~user/.ssh/authorized_keys if it doesn't exist. *)
+  let auth_keys = sprintf "%s/authorized_keys" ssh_dir in
+  if not (g#exists auth_keys) then (
+    g#touch auth_keys;
+    g#chmod 0o644 auth_keys
+  );
+
+  (* Append the key. *)
+  g#write_append auth_keys key

customize/ssh_key.mli
@@ -1,0 +1,32 @@
+(* virt-customize
+ * Copyright (C) 2014 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+type ssh_key_selector =
+| SystemKey                    (* Default key from the user in the system, in
+                                * the style of ssh-copy-id(1)/default_ID_file.
+                                *)
+| KeyFile of string            (* Key from the specified file. *)
+| KeyString of string          (* Key specified as string. *)
+
+val parse_selector : string -> ssh_key_selector
+(** Parse the selector field in --ssh-inject.  Note this
+    doesn't parse the username part.  Exits if the format is not valid. *)
+
+val do_ssh_inject_unix : Guestfs.guestfs -> string -> ssh_key_selector -> unit
+(** Inject on a generic Unix system (Linux, FreeBSD, etc) the ssh key
+    for the specified user. *)

po/POTFILES-ml
@@ -25,6 +25,7 @@ customize/hostname.ml
 customize/password.ml
 customize/perl_edit.ml
 customize/random_seed.ml
+customize/ssh_key.ml
 customize/timezone.ml
 customize/urandom.ml
 mllib/JSON.ml

commit c78e1ffa0ac7202af20c2ec26157e722b4104b2e

customize/customize_run.ml: Fix missing word in warning
Reported-by: Dominik Holler <dholler@redhat.com>
Fixes: commit d42aa6d273879d3f0b0039139775b882517073ac

customize/customize_run.ml
@@ -256,7 +256,7 @@ let run (g : G.guestfs) root (ops : ops) =
         message (f_"SSH key inject: %s") user;
         Ssh_key.do_ssh_inject_unix g user selector
       ) else
-        warning (f_"SSH key could be injected for this type of guest")
+        warning (f_"SSH key could not be injected for this type of guest")
 
     | `Truncate path ->
       message (f_"Truncating: %s") path;