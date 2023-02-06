libguestfs/guestfs-tools

commit 11fee3f83fb0ad37f17f34aad3fceb953cf142c0

New 'virt-drivers' tool
This tool can be pointed at a disk image which lacks metadata and in
some circumstances can determine useful information about it, such as
what bootloader it contains and what device emulation it needs to
boot.

Currently it only works for Linux guests.  Windows guests TBD, which
will involve examining the Windows Registry DriverDatabase.

In almost all circumstances it is better to attach metadata to your
disk images, but this tool accepts the reality that there are many
broken distribution systems out there that distribute disk images
without metadata (including some written by Red Hat).

See also
https://listman.redhat.com/archives/libguestfs/2022-December/030389.html

drivers/drivers.ml
@@ -1,0 +1,248 @@
+(* virt-drivers
+ * Copyright (C) 2009-2023 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *)
+
+open Std_utils
+open Tools_utils
+open Common_gettext.Gettext
+open Getopt.OptionName
+open DOM
+
+open Printf
+
+let parse_cmdline () =
+  let blocksize = ref 0 in
+  let domain = ref None in
+  let file = ref None in
+  let libvirturi = ref "" in
+  let format = ref "auto" in
+
+  let set_file arg =
+    if !file <> None then
+      error (f_"--add option can only be given once");
+    let uri =
+      try URI.parse_uri arg
+      with URI.Parse_failed ->
+        error (f_"error parsing URI '%s'. Look for error messages printed above.") arg in
+    file := Some uri
+  and set_domain dom =
+    if !domain <> None then
+      error (f_"--domain option can only be given once");
+    domain := Some dom
+  in
+
+  let argspec = [
+    [ S 'a'; L"add" ], Getopt.String (s_"file", set_file), s_"Add disk image file";
+    [ L"blocksize" ], Getopt.Set_int ("512|4096", blocksize), s_"Set disk sector size";
+    [ S 'c'; L"connect" ], Getopt.Set_string (s_"uri", libvirturi), s_"Set libvirt URI";
+    [ S 'd'; L"domain" ], Getopt.String (s_"domain", set_domain), s_"Set libvirt guest name";
+    [ L"format" ], Getopt.Set_string (s_"format", format), s_"Format of input disk";
+  ] in
+  let usage_msg =
+    sprintf (f_"\
+%s: detect bootloader, kernel and drivers inside guest
+
+A short summary of the options is given below.  For detailed help please
+read the man page virt-drivers(1).
+")
+      prog in
+  let opthandle = create_standard_options argspec ~key_opts:true usage_msg in
+  Getopt.parse opthandle.getopt;
+
+  (* Check -a and -d options. *)
+  let file = !file in
+  let domain = !domain in
+  let libvirturi = match !libvirturi with "" -> None | s -> Some s in
+  let add =
+    match file, domain with
+    | None, None ->
+      error (f_"you must give either -a or -d options.  Read virt-get-kernel(1) man page for further information.")
+    | Some _, Some _ ->
+      error (f_"you cannot give -a and -d options together.  Read virt-get-kernel(1) man page for further information.")
+    | None, Some dom ->
+      fun (g : Guestfs.guestfs) ->
+        let readonlydisk = "ignore" (* ignore CDs, data drives *) in
+        ignore (g#add_domain
+                  ~readonly:true ~allowuuid:true ~readonlydisk
+                  ?libvirturi dom)
+    | Some uri, None ->
+      fun g ->
+        let { URI.path; protocol; server; username; password } = uri in
+        let format = match !format with "auto" -> None | s -> Some s in
+        let blocksize = match !blocksize with 0 -> None | i -> Some i in
+        g#add_drive
+          ~readonly:true ?blocksize ?format ~protocol ?server ?username
+          ?secret:password path
+  in
+
+  add, opthandle.ks
+
+let rec do_detection g roots =
+  let comment = Comment generated_by in
+  let firmware, firmware_xml = do_detect_firmware g in
+  let oses = List.map (fun root -> do_detect_os g root firmware) roots in
+  let doc : DOM.doc =
+    doc "operatingsystems" [] (comment :: firmware_xml @ oses) in
+  doc
+
+and do_detect_firmware g =
+  let firmware = Firmware.detect_firmware g in
+  let xml =
+    match firmware with
+    | Firmware.I_BIOS ->
+       [ e "firmware" ["type", "bios"] [] ]
+    | Firmware.I_UEFI esps ->
+       List.map (fun esp -> e "firmware" ["type", "uefi"] [ PCData esp ])
+         esps in
+  firmware, xml
+
+and do_detect_os g root firmware =
+  let body = ref [] in
+
+  (* Display some of the standard virt-inspector fields. *)
+  List.push_back body (e "root" [] [ PCData root ]);
+  let typ = g#inspect_get_type root in
+  if typ <> "unknown" then
+    List.push_back body (e "name" [] [ PCData typ ]);
+
+  let adds fn field =
+    let v = fn root in
+    if v <> "unknown" then
+      List.push_back body (e field [] [ PCData v ]);
+  and addi fn field =
+    let v = fn root in
+    List.push_back body (e field [] [ PCData (string_of_int v) ]);
+  in
+  adds g#inspect_get_arch               "arch";
+  adds g#inspect_get_distro             "distro";
+  adds g#inspect_get_product_name       "product_name";
+  adds g#inspect_get_product_variant    "product_variant";
+  addi g#inspect_get_major_version      "major_version";
+  addi g#inspect_get_minor_version      "minor_version";
+  adds g#inspect_get_package_format     "package_format";
+  adds g#inspect_get_package_management "package_management";
+  (* XXX build_id was added in libguestfs 1.49.8.  Wait until this is
+   * available in a stable release of libguestfs, ie. 1.50, before
+   * uncommenting this.
+   *)
+  (*adds g#inspect_get_build_id         "build_id";*)
+  adds g#inspect_get_osinfo             "osinfo";
+
+  (* Now mount up the disks in order to detect bootloader and kernels. *)
+  let mps = g#inspect_get_mountpoints root in
+  let cmp (a,_) (b,_) = compare (String.length a) (String.length b) in
+  let mps = List.sort cmp mps in
+  List.iter (fun (mp, dev) -> g#mount_ro dev mp) mps;
+
+  (match typ with
+   | "linux" ->
+      (* XXX This shouldn't be necessary.  Linux_* modules should do it. *)
+      g#aug_init "/" 1;
+      let bootloader = do_detect_linux_bootloader g root firmware in
+      List.push_back body bootloader
+   | _ -> () (* XXX TBD *)
+  );
+
+  g#umount_all ();
+
+  e "operatingsystem" [] !body
+
+and do_detect_linux_bootloader g root firmware =
+  let bootloader = Linux_bootloaders.detect_bootloader g root firmware in
+  let bl_name = bootloader#name in
+  let bl_config = bootloader#get_config_file () in
+  let kernels = do_detect_linux_kernels g root bootloader in
+  e "bootloader" ["type", bl_name; "config", bl_config] kernels
+
+and do_detect_linux_kernels g root bootloader =
+  let apps = g#inspect_list_applications2 root in
+  let apps = Array.to_list apps in
+  let kernels = Linux_kernels.detect_kernels g root bootloader apps in
+  List.map kernel_info_to_xml kernels
+
+and kernel_info_to_xml { Linux_kernels.ki_name; ki_version;
+                         ki_arch; ki_vmlinuz; ki_initrd; ki_modpath;
+                         ki_modules; ki_supports_virtio_blk;
+                         ki_supports_virtio_net; ki_supports_virtio_rng;
+                         ki_supports_virtio_balloon;
+                         ki_supports_isa_pvpanic;
+                         ki_supports_virtio_socket;
+                         ki_is_xen_pv_only_kernel;
+                         ki_is_debug; ki_config_file } =
+  let body = ref [] in
+  List.push_back body (e "name" []    [ PCData ki_name ]);
+  List.push_back body (e "version" [] [ PCData ki_version ]);
+  List.push_back body (e "arch" []    [ PCData ki_arch ]);
+  List.push_back body (e "vmlinuz" [] [ PCData ki_vmlinuz ]);
+  Option.may
+    (fun v -> List.push_back body (e "initrd" []  [ PCData v ]))
+    ki_initrd;
+  List.push_back body (e "modules_path" [] [ PCData ki_modpath ]);
+  List.push_back body (e "modules" []
+                         (List.map (fun m -> e "module" [] [ PCData m ])
+                            (List.sort compare ki_modules)));
+  if ki_supports_virtio_blk then
+    List.push_back body (e "supports_virtio_blk" [] []);
+  if ki_supports_virtio_net then
+    List.push_back body (e "supports_virtio_net" [] []);
+  if ki_supports_virtio_rng then
+    List.push_back body (e "supports_virtio_rng" [] []);
+  if ki_supports_virtio_balloon then
+    List.push_back body (e "supports_virtio_balloon" [] []);
+  if ki_supports_isa_pvpanic then
+    List.push_back body (e "supports_isa_pvpanic" [] []);
+  if ki_supports_virtio_socket then
+    List.push_back body (e "supports_virtio_socket" [] []);
+  if ki_is_xen_pv_only_kernel then
+    List.push_back body (e "is_xen_pv_only_kernel" [] []);
+  if ki_is_debug then
+    List.push_back body (e "debug_kernel" [] []);
+  Option.may
+    (fun v -> List.push_back body (e "config_file" []  [ PCData v ]))
+    ki_config_file;
+
+  e "kernel" [] !body
+
+(* Main program. *)
+let main () =
+  let add, ks = parse_cmdline () in
+
+  (* Connect to libguestfs. *)
+  let g = open_guestfs () in
+  add g;
+  g#set_network (key_store_requires_network ks);
+  g#launch ();
+
+  (* Decrypt the disks. *)
+  inspect_decrypt g ks;
+
+  let roots = g#inspect_os () in
+  let roots = Array.to_list roots in
+
+  (* Can't call inspect_mount_root here (ie. normal processing of
+   * the -i option) because it can only handle a single root.
+   *)
+
+  (* Do the detection. *)
+  let doc : DOM.doc = do_detection g roots in
+  DOM.doc_to_chan stdout doc;
+
+  (* Shutdown. *)
+  g#shutdown ();
+  g#close ()
+
+let () = run_main_and_handle_errors main

.gitignore
@@ -32,6 +32,7 @@ Makefile.in
 /bash/virt-df
 /bash/virt-dib
 /bash/virt-diff
+/bash/virt-drivers
 /bash/virt-edit
 /bash/virt-filesystems
 /bash/virt-format
@@ -97,6 +98,8 @@ Makefile.in
 /dib/.depend
 /dib/output_format_*.mli
 /dib/virt-dib
+/drivers/.depend
+/drivers/virt-drivers
 /diff/virt-diff
 /edit/virt-edit
 /format/virt-format

Makefile.am
@@ -40,6 +40,7 @@ SUBDIRS += common/mlprogress
 SUBDIRS += common/mlvisit
 SUBDIRS += common/mlxml
 SUBDIRS += common/mltools
+SUBDIRS += common/mldrivers
 SUBDIRS += common/mlcustomize
 endif
 
@@ -54,6 +55,7 @@ if HAVE_OCAML
 # used by other OCaml tools, so these must come first.
 SUBDIRS += customize
 SUBDIRS += builder builder/templates
+SUBDIRS += drivers
 SUBDIRS += get-kernel
 SUBDIRS += resize
 SUBDIRS += sparsify
@@ -119,7 +121,7 @@ po/POTFILES: configure.ac
 po/POTFILES-ml: configure.ac
 	rm -f $@ $@-t
 	cd $(srcdir); \
-	find builder common/ml* customize dib get-kernel resize sparsify sysprep -name '*.ml' | \
+	find builder common/ml* customize dib drivers get-kernel resize sparsify sysprep -name '*.ml' | \
 	grep -v '^builder/templates/' | \
 	grep -v '^common/mlv2v/' | \
 	grep -v -E '.*_tests\.ml$$' | \

bash/Makefile.am
@@ -29,6 +29,7 @@ symlinks = \
 	virt-diff \
 	virt-df \
 	virt-dib \
+	virt-drivers \
 	virt-edit \
 	virt-filesystems \
 	virt-format \
@@ -54,7 +55,7 @@ CLEANFILES += \
 # common options like -d is handled by this common script.  However
 # this script cannot deal with commands that use --ro/--rw
 # (eg. virt-rescue).  Those tools have to be handled individually.
-virt-builder virt-cat virt-customize virt-df virt-dib virt-diff \
+virt-builder virt-cat virt-customize virt-df virt-dib virt-diff virt-drivers \
 virt-edit virt-filesystems virt-format virt-get-kernel virt-inspector \
 virt-log virt-ls \
 virt-resize virt-sparsify virt-sysprep \

bash/virt-alignment-scan
@@ -127,6 +127,12 @@ _virt_diff ()
 } &&
 complete -o default -F _virt_diff virt-diff
 
+_virt_drivers ()
+{
+    _guestfs_virttools "virt-drivers" 1
+} &&
+complete -o default -F _virt_drivers virt-drivers
+
 _virt_edit ()
 {
     _guestfs_virttools "virt-edit" 0

cat/virt-filesystems.pod
@@ -388,6 +388,7 @@ L<guestfs(3)>,
 L<guestfish(1)>,
 L<virt-cat(1)>,
 L<virt-df(1)>,
+L<virt-drivers(1)>,
 L<virt-list-filesystems(1)>,
 L<virt-list-partitions(1)>,
 L<csvtool(1)>,

configure.ac
@@ -115,6 +115,7 @@ AC_CONFIG_FILES([Makefile
                  cat/Makefile
                  common/edit/Makefile
                  common/mlcustomize/Makefile
+                 common/mldrivers/Makefile
                  common/mlgettext/Makefile
                  common/mlpcre/Makefile
                  common/mlprogress/Makefile
@@ -137,6 +138,7 @@ AC_CONFIG_FILES([Makefile
                  df/Makefile
                  dib/Makefile
                  diff/Makefile
+                 drivers/Makefile
                  edit/Makefile
                  format/Makefile
                  get-kernel/Makefile

df/virt-df.pod
@@ -260,6 +260,7 @@ error.
 L<df(1)>,
 L<guestfs(3)>,
 L<guestfish(1)>,
+L<virt-drivers(1)>,
 L<virt-filesystems(1)>,
 L<http://libguestfs.org/>.
 

commit 2d26b8e30b4133ecb1eeaa1d221110d281155d3a

drivers: Fix reference to manual page in error message
When pasting this code from virt-get-kernel I forgot the change the
man page name in the error message.

Fixes: commit 11fee3f83fb0ad37f17f34aad3fceb953cf142c0

drivers/drivers.ml
@@ -70,9 +70,11 @@ read the man page virt-drivers(1).
   let add =
     match file, domain with
     | None, None ->
-      error (f_"you must give either -a or -d options.  Read virt-get-kernel(1) man page for further information.")
+      error (f_"you must give either -a or -d options.  \
+                Read virt-drivers(1) man page for further information.")
     | Some _, Some _ ->
-      error (f_"you cannot give -a and -d options together.  Read virt-get-kernel(1) man page for further information.")
+      error (f_"you cannot give -a and -d options together.  \
+                Read virt-drivers(1) man page for further information.")
     | None, Some dom ->
       fun (g : Guestfs.guestfs) ->
         let readonlydisk = "ignore" (* ignore CDs, data drives *) in