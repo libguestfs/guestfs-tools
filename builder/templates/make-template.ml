25bc57a0ef3491fe11a77beadce863c3e1d8c053
Richard W.M. Jones <rjones@redhat.com>
2026-04-13 08:48:37 +0100

    builder/templates/make-template.ml: Fix RHEL 10 URL
    Switch to local mirror.
    
    Switch away from RHEL 10 Beta and generalize the URL to work for all
    RHEL 10 minor releases.

--- builder/templates/make-template.ml
+++ builder/templates/make-template.ml
@@ -986,13 +986,10 @@ and make_boot_media os arch =
                         rhel-9/RHEL-9/9.%d.0/BaseOS/%s/os"
                  minor (string_of_arch arch))
 
-  (* This is RHEL 10.0 Beta, will be replaced with RHEL 10.0 when
-   * that is released later.
-   *)
-  | RHEL (10, 0), arch ->
-     Location (sprintf "http://download.eng.bos.redhat.com/released/\
-                        rhel-10/RHEL-10/10.0-Beta-0/BaseOS/%s/os/"
-                 (string_of_arch arch))
+  | RHEL (10, minor), arch ->
+     Location (sprintf "http://download.eng.brq.redhat.com/released/\
+                        rhel-10/RHEL-10/10.%d/BaseOS/%s/os/"
+                 minor (string_of_arch arch))
 
   | Ubuntu (_, dist), X86_64 ->
      Location (sprintf "http://archive.ubuntu.com/ubuntu/dists/\
